4d7cfab8b5813bcbdaf6cdd5780ba2bc3c2112f1
kyle67 <daniels.kyle@gmail.com>
2020-01-13 17:57:25 -0500

    Modified server.js , routes, and css

---
+++ controllers/note.js
@@ -0,0 +1,22 @@
+var db = require("../models");
+
+module.exports = {
+  // Find one note
+  find: function(req, res) {
+    db.Note.find({ _headlineId: req.params.id }).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  },
+  // Create a new note
+  create: function(req, res) {
+    db.Note.create(req.body).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  },
+  //Remove note
+  delete: function(req, res) {
+    db.Note.remove({ _id: req.params.id }).then(function(dbNote) {
+      res.json(dbNote);
+    });
+  }
+};
--- server.js
+++ server.js
@@ -1,93 +1,37 @@
+// Require dependencies
 var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
+// Set port or use 3000
+var PORT = process.env.PORT || 3000;
 
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-var db = require("./models");
-
-var PORT = 3000;
-
+// Rename express as app
 var app = express();
 
+// Require  routes
+var routes = require("./routes");
 
-app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/week18Populater", {
-    useMongoClient: true
-});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 
-app.post("/scrape", function(req, res) {
-    axios.get("http://www.echojs.com/").then(function(response) {
-        var $ = cheerio.load(response.data);
+app.use(routes);
 
-        $("article h2").each(function(i, element) {
-            var result = {};
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
-            db.Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                })
-                .catch(function(err) {
-                    return res.json(err);
-                });
-        });
-
-        res.send("Scrape Complete");
-    });
-});
-
-app.get("/articles", function(req, res) {
-    db.Article.find({})
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-app.get("/articles/:id", function(req, res) {
-    db.Article.findOne({ _id: req.params.id })
-        .populate("note")
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-            res.json(err);
-        });
-});
-
-app.post("/articles/:id", function(req, res) {
-    db.Note.create(req.body)
-        .then(function(dbNote) {
-            return db.Article.update({ _id: req.params.id }, { $push: { notes: dbNote } });
-        })
-        .then(function(dbArticle) {
-            res.json(dbArticle);
-        })
-        .catch(function(err) {
-
-            res.json(err);
-        });
-});
-
-
+// Listen on the port
 app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
+  console.log("Listening on port: " + PORT);
 });

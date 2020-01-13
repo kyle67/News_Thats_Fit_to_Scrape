f8d98cad34d60d4f0ce9de5041d32add1bb5e2de
kyle67 <daniels.kyle@gmail.com>
2020-01-13 15:00:03 -0500

    Modified server.js and removed handlebars

--- server.js
+++ server.js
@@ -1,128 +1,93 @@
-// Dependencies
-var mongojs = require("mongojs");
-var cheerio = require("cheerio");
-var axios = require("axios");
 var express = require("express");
-var exphbs = require('express-handlebars');
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
 
 var app = express();
-var PORT = process.env.PORT || 3000
+
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-// Set Handlebars as the default templating engine.
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Database configuration
-var collections = ["scrapedData"];
-
-// Hook mongojs configuration to the db variable
-var db = mongojs(process.env.MONGODB_URI || 'scraper', collections);
-db.on("error", function (error) {
-  console.log("Database Error:", error);
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/week18Populater", {
+    useMongoClient: true
 });
 
-// Main route, render index.handlebars
-app.get("/", function(req, res) {
-  res.render("index");
-})
 
-// Make a request via axios to grab the HTML body from the site of your choice
-app.get("/scrape", function (req, res) {
+app.post("/scrape", function(req, res) {
+    axios.get("http://www.echojs.com/").then(function(response) {
+        var $ = cheerio.load(response.data);
 
-  db.scrapedData.drop()
+        $("article h2").each(function(i, element) {
+            var result = {};
 
-  axios
-    .get("https://kotaku.com/")
-    .then(function (response) {
-      // Load the HTML into cheerio and save it to a variable
-      // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-      var $ = cheerio.load(response.data);
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
 
-      // An empty array to save the data that we'll scrape
-      var results = [];
-
-      // Select each element in the HTML body from which you want information.
-      // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-      // but be sure to visit the package's npm page to see how it works
-      $("article").each(function (i, element) {
-        var title = $(element)
-          .find("h1")
-          .children("a")
-          .text();
-        var link = $(element)
-          .find("h1")
-          .children("a")
-          .attr("href");
-        var image = $(element)
-          .find("source")
-          .attr("data-srcset");
-        var summary = $(element)
-          .find("p").text();
-
-       // If this found element had both a title and a link
-       if (title && link && image && summary) {
-
-        
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link,
-          image: image,
-          summary: summary
-        }, 
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log("scrapedData")
-            console.log(inserted);
-          }
+            db.Article.create(result)
+                .then(function(dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function(err) {
+                    return res.json(err);
+                });
         });
-      }
-      });
 
-      // Log the results once you've looped through each of the elements found with cheerio
-      console.log(results);
-    })
+        res.send("Scrape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+        .populate("note")
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+            res.json(err);
+        });
+});
+
+app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.update({ _id: req.params.id }, { $push: { notes: dbNote } });
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function(err) {
+
+            res.json(err);
+        });
 });
 
 
-// route 1
-app.get("/all", function (req, res) {
-  db.scrapedData.find({}, function (err, found) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.json(found)
-    }
-  });
-});
-
-
-//title route 
-app.get("/title", function(req, res) {
- 
-  db.scrapedData.find().sort({ title: 1 }, function(error, found) {
-    // Log any errors if the server encounters one
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the result of this query to the browser
-    else {
-      res.send(found);
-    }
-  });
-});
-
-
-
-
-
-app.listen(PORT, function () {
-  console.log("App running on port 3000!");
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
 });
